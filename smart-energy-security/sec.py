4d4412b7819d3024628d213615f0da344a22b310
Andreea Nechita <an2u18@soton.ac.uk>
2021-05-05 15:07:05 +0100

    Implemented scheduling based on pricing data label

--- smart-energy-security/sec.py
+++ smart-energy-security/sec.py
@@ -12,8 +12,11 @@ import scheduler.scheduler as sd
 
 
 def main():
+    os.mkdir(os.path.join(os.path.dirname(Path(__file__)), '../out'))
     resources_path = os.path.normpath(
         os.path.join(os.path.dirname(Path(__file__)), '../resources'))
+    out_path = os.path.normpath(
+        os.path.join(os.path.dirname(Path(__file__)), '../out'))
 
     parser = argparse.ArgumentParser()
     detect_args = parser.add_argument_group('Detection arguments',
@@ -62,9 +65,11 @@ def main():
                                         sys.argv,
                                help='file containing scheduling requirements '
                                     'that has to be satisfied')
-    schedule_args.add_argument('-p', '--pricing', type=str, required=(
-                                                                                 '--schedule' in sys.argv or '-S' in sys.argv) and not (
-                '--detect' in sys.argv or '-D' in sys.argv),
+    schedule_args.add_argument('-p', '--pricing', type=str,
+                               required=('--schedule' in sys.argv or '-S' in
+                                         sys.argv) and not ('--detect' in
+                                                            sys.argv or '-D'
+                                                            in sys.argv),
                                help='file containing pricing')
     schedule_args.add_argument('-l', '--label',
                                choices=['none', 'normal', 'abnormal', 'all'],
@@ -82,10 +87,14 @@ def main():
         scaled_data = scaler.transform(price_data)
         prediction = hm.classify_price(model, scaled_data.reshape(
             scaled_data.shape[0], 24, 1)).flatten()
-        predicted_data = pd.concat([price_data, pd.Series(prediction)], axis=1)
-        predicted_data.to_csv('TestingResults.txt', header=False, index=False)
+        predicted_data = pd.concat([price_data, pd.Series(prediction,
+                                                          name='label')],
+                                   axis=1)
+        predicted_data.to_csv(os.path.join(out_path, 'TestingResults.txt'),
+                              header=False, index=False)
 
     if args.schedule:
+        os.mkdir(os.path.join(out_path, 'figures'))
         if args.pricing:
             columns = list(map(str, range(24)))
             if args.label != 'none':
@@ -93,12 +102,20 @@ def main():
             cost = pd.read_table(args.pricing, sep=',', names=columns)
         elif args.detect:
             cost = predicted_data
+        if args.label == 'normal':
+            cost = cost.loc[cost['label'] == 0]
+        elif args.label == 'abnormal':
+            cost = cost.loc[cost['label'] == 1]
+        if 'label' in cost.columns:
+            cost.drop(columns=['label'], inplace=True)
         requirements = pd.read_excel(args.requirements)
         for idx, c in cost.iterrows():
             results = sd.schedule(requirements,
                                   cost=c.to_numpy().flatten().tolist())
             results_matrix = results.x.reshape(len(results.x) // 24, 24)
-            sd.plot_consumption(results_matrix.T, 'fig' + str(idx) + '.png')
+            sd.plot_consumption(results_matrix.T,
+                                os.path.join(out_path, 'figures',
+                                             'fig' + str(idx) + '.png'))
             print(results)
 
 
