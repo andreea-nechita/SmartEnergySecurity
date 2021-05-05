ab243f1bec3ca92e907c3999293e223ea9dd0470
Andreea Nechita <an2u18@soton.ac.uk>
2021-05-06 00:42:12 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='sec', version='1.0', packages=[''], url='', license='',
+      author='andreeanechita', author_email='an2u18@soton.ac.uk',
+      description='SmartEnergySecurity for smart scheduling and detection of '
+                  'manipulated pricing')
